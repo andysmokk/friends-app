a63861ccfba434830415aea0a2d8a8c7d1a9a49f
Andrii Konovalov <andy.smokk@gmail.com>
2022-09-29 15:15:02 +0200

    styles done

--- js/styles.js
+++ js/styles.js
@@ -1,8 +1,10 @@
 const addClassActive = (inputs) => {
   inputs.forEach((input) => {
     input.checked
-      ? input.labels[0].classList.add("active")
-      : input.labels[0].classList.remove("active");
+      ? (input.labels[0].classList.add("active"),
+        input.labels[0].children[0].classList.add("active"))
+      : (input.labels[0].classList.remove("active"),
+        input.labels[0].children[0].classList.remove("active"));
   });
 };
 
